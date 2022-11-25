1c6de365abf77811c0f66166e3d6a7247b32193c
Justin Holzer <justin@jtcholzer.net>
2022-11-25 07:04:22 -0700

    Add WebAssembly version of website.

---
+++ Team60WebAssembly/Data/LinksSource.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace KingmanAzFrcTeam60.Data
+{
+    public class Link
+    {
+        public Link(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        public string HRef
+        {
+            get
+            {
+                var str = String.Format("<a href=\"{0}\">{0}</a>", Url);
+                return str;
+            }
+        }
+    }
+
+    public class LinksSource
+    {
+        public Link[]? Links { get; set; }
+
+        public async Task PopulateLinks()
+        {
+            try
+            {
+                var localDir = System.IO.Directory.GetCurrentDirectory();
+                var path = Path.Combine(localDir, "wwwroot", "Links.json");
+                var linksFile = new FileStream(path, FileMode.Open, FileAccess.Read);
+                Links = await JsonSerializer.DeserializeAsync<Link[]>(linksFile);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Team60WebAssembly/Data/Sponsor.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace KingmanAzFrcTeam60.Data
+{
+    public class SponsorsList
+    {
+        [JsonPropertyName("sponsors")]
+        public Sponsor[]? Sponsors;
+    }
+
+    public class Sponsor
+    {
+        [JsonPropertyName("businessName")]
+        public string? BusinessName { get; set; }
+
+        [JsonPropertyName("website")]
+        public string? Website { get; set; }
+
+        [JsonPropertyName("years")]
+        public int[]? Years { get; set; }
+
+        public string AllYears => Years == null ? "" : String.Join(", ", Years);
+
+        public string GetWebsite => Website == null ? "" : Website == "null" ? "" : Website;
+
+    }
+
+
+    public class SponorsSource
+    {
+        public Sponsor[]? Sponsors = null;
+        public async Task PopulateSponsors()
+        {
+            try
+            {
+                var localDir = System.IO.Directory.GetCurrentDirectory();
+                var path = Path.Combine(localDir, "wwwroot", "Sponsors2.json");
+                var sponsorsFile = new FileStream(path, FileMode.Open, FileAccess.Read);
+                Sponsors = await JsonSerializer.DeserializeAsync<Sponsor[]>(sponsorsFile);
+                if (Sponsors != null)
+                {
+                    Array.Sort(Sponsors, (lh, rh) => String.Compare(lh.BusinessName, rh.BusinessName));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
