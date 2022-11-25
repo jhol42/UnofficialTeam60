4349d25da9443c2bf64fd108a8755102365b5013
Justin Holzer <justin@jtcholzer.net>
2022-11-25 09:34:13 -0700

    Fixup Docker build/push scripts.

--- KingmanAzFrcTeam60/PushDev.ps1
+++ KingmanAzFrcTeam60/PushDev.ps1
@@ -1,5 +1,4 @@
 az login
 $TOKEN=$(az acr login --name team60.azurecr.io --expose-token --output tsv --query accessToken)
 docker login team60.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
-docker tag team60.azurecr.io/kingmanazfrcteam60:dev team60.azurecr.io/kingmanazfrcteam60:latest
-docker push 
+docker push team60.azurecr.io/kingmanazfrcteam60:dev
--- KingmanAzFrcTeam60/TagPushLatest.ps1
+++ KingmanAzFrcTeam60/TagPushLatest.ps1
@@ -1,4 +1,5 @@
 az login
 $TOKEN=$(az acr login --name team60.azurecr.io --expose-token --output tsv --query accessToken)
 docker login team60.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
-docker push team60.azurecr.io/kingmanazfrcteam60:dev
+docker tag team60.azurecr.io/kingmanazfrcteam60:dev team60.azurecr.io/kingmanazfrcteam60:latest
+docker push team60.azurecr.io/kingmanazfrcteam60:latest
